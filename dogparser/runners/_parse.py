5bc7a8e7ac06c7337fac7afd7d495efadb1bb8ca
Sindre Osnes <sindreosnes.git@gmail.com>
2022-11-19 12:02:01 +0100

    Add arguments to cli

--- dogparser/runners/_parse.py
+++ dogparser/runners/_parse.py
@@ -3,5 +3,8 @@ import os
 
 def parse():
     argument_parser = argparse.ArgumentParser(description='CLI interface for parsing DataEase files')
+    argument_parser.add_argument('TABLE', help='The dataease table to parse, defines the logic')
+    argument_parser.add_argument('SOURCE_FILE', help='The dataease file to parse')
+    argument_parser.add_argument('DESTINATION_FOLDER', help='The destination directory')
 
     print('Hello World')
