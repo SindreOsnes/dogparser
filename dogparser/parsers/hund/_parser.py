b804df76b3cad2e92cb63661a5759bb149b5ff2e
Sindre Osnes <sindreosnes.git@gmail.com>
2022-11-19 13:55:14 +0100

    Add country extraction to parser

--- dogparser/parsers/hund/_parser.py
+++ dogparser/parsers/hund/_parser.py
@@ -1,4 +1,6 @@
-
+import json
+import os
+from typing import List, Tuple
 
 def parse(source_definition: str, destination_folder: str):
     
@@ -8,4 +10,30 @@ def parse(source_definition: str, destination_folder: str):
     with open(schema_file, 'rb') as f:
         schema_data = f.read()
     
-    print(schema_data)
+    # Strip the extraneous data and split into elements
+    stripped_schema_data = schema_data[schema_data.index(b'nei\x00ja'):]
+    schema_split = stripped_schema_data.split(b'\x00')
+
+    # Get the country enumerator
+    land_enum, schema_split = extract_enum(schema_split, b'LAND')
+
+    with open(os.path.join(destination_folder, 'LAND.json'), 'w') as f:
+        json.dump(land_enum, f, ensure_ascii=False, indent=True)
+
+    print(schema_split)
+
+def extract_enum(elements: List[bytes], query_term: bytes) -> Tuple[List[str], List[bytes]]:
+    query_idx = elements.index(query_term)
+    query_len = elements[query_idx+1][0]
+    
+    print(f'{query_term} contains {query_len+1} entries including leading NULL')
+
+    query = elements[query_idx+1:query_idx+query_len+1]
+
+    enum = [''] + [graceful_conversion(x) for x in query]
+
+    return enum, elements[query_idx+query_len+1:]
+
+def graceful_conversion(input_element: bytes) -> str:
+    data_out = input_element
+    return data_out.decode('utf8')
