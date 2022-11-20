72ce4a6cfd3414b19935997e5620ea355630eced
Sindre Osnes <sindreosnes.git@gmail.com>
2022-11-20 15:59:53 +0100

    Add initial date conversion

--- dogparser/utils/_conversion.py
+++ dogparser/utils/_conversion.py
@@ -1,3 +1,6 @@
+from typing import Union
+from datetime import date
+
 def graceful_conversion(input_element: bytes) -> str:
     data_out = input_element
     data_out = sure_conversion(data_out)
@@ -24,3 +27,23 @@ def uncertain_conversions(input_element: bytes) -> bytes:
     data_out = data_out.replace(b'\x9a', b'?x9a') # HUNDER (reg_nr: SZNIXL?x9aF, value(reg_nr): SZNIXL?x9aF)
     
     return data_out
+
+def date_conversion(data: bytes) -> Union[str, None]:
+    datestr = graceful_conversion(data)
+    if len(datestr) != 6:
+        return None
+    else:
+        try:
+            year = int(datestr[-2:])
+            if year > 25:
+                year += 1900
+            else:
+                year += 2000
+
+            month = int(datestr[2:-2])
+            day = int(datestr[0:2])
+
+            return date(day=day, month=month, year=year).isoformat()
+        except Exception as e:
+            print(datestr, data)
+            raise e
