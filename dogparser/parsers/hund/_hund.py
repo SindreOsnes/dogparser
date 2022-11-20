6a1e70ac6b3fb37d4baf190beb901e1fb42923ea
Sindre Osnes <sindreosnes.git@gmail.com>
2022-11-20 17:28:33 +0100

    Add litter number

--- dogparser/parsers/hund/_hund.py
+++ dogparser/parsers/hund/_hund.py
@@ -10,12 +10,15 @@ class Hund:
     _fodt_raw: Union[date, None] # Birthdate raw value (FØDT in original schema)
     _fd_land_id: int # Birth country
     _kjonn_id: int # Sex
+    _kullnr: Union[str, None] # Litter identifier
 
-    def __init__(self, reg_nr: str, fd_land_id: int, kjonn_id: int, fodt: Union[date, None] = None, fodt_raw: Union[str, None] = None) -> None:
+    def __init__(self, reg_nr: str, fd_land_id: int, kjonn_id: int,
+                 fodt: Union[date, None] = None, fodt_raw: Union[str, None] = None, kullnr: Union[str, None] = None) -> None:
         self._reg_nr = reg_nr
         self._fodt = fodt
         self._fd_land_id = fd_land_id
         self._kjonn_id = kjonn_id
+        self._kullnr = kullnr
         self._fodt_raw = fodt_raw
     
     @classmethod
@@ -28,7 +31,7 @@ class Hund:
 
         # Eliminate the already used data and set the next property
         sub_content = content[10:] # 240 bytes remaining
-        reg_nr = graceful_conversion(sub_content[:12]) # REG.NR (registration number is a string capped at 12 character)
+        reg_nr = graceful_conversion(sub_content[:12]) # REG.NR (registration number is a string capped at 12 characters)
 
         # Eliminate the already used data and set the next property
         sub_content = sub_content[12:] # 228 bytes remaining
@@ -46,8 +49,12 @@ class Hund:
         # Eliminate the already used data and set the next property
         sub_content = sub_content[1:] # 221 bytes remaining
         kjonn_id = sub_content[0]
+        
+        # Eliminate the already used data and set the next property
+        sub_content = sub_content[1:] # 220 bytes remaining
+        kullnr = graceful_conversion(sub_content[:5]) # KULLNR (litter number/identifier is a string capped at 5 characters)
 
-        return cls(reg_nr=reg_nr, fodt=fodt, fodt_raw=fodt_raw, fd_land_id=fd_land_id, kjonn_id=kjonn_id)
+        return cls(reg_nr=reg_nr, fodt=fodt, fodt_raw=fodt_raw, fd_land_id=fd_land_id, kjonn_id=kjonn_id, kullnr=kullnr)
     
     @property
     def reg_nr(self) -> str:
@@ -67,7 +74,11 @@ class Hund:
     
     @property
     def fodt_str(self) -> Union[str, None]:
-        return self._fodt if not self._fodt else self.fodt.isoformat()
+        return None if not self._fodt else self.fodt.isoformat()
+    
+    @property
+    def kullnr(self) -> Union[str, None]:
+        return None if not self._kullnr else self._kullnr
     
     @property
     def fodt_raw(self) -> Union[str, None]:
@@ -77,7 +88,14 @@ class Hund:
     def native(self) -> dict:
         """Method converts class instance to native python classes for serialization purposes"""
 
-        return {'reg_nr': self.reg_nr, 'fodt': self.fodt_str, 'fodt_raw': self.fodt_raw, 'fd_land_id': self._fd_land_id, 'kjonn_id': self.kjonn_id}
+        return {
+            'reg_nr': self.reg_nr,
+            'fodt': self.fodt_str,
+            'fodt_raw': self.fodt_raw,
+            'fd_land_id': self._fd_land_id,
+            'kjonn_id': self.kjonn_id,
+            'kullnr': self.kullnr
+            }
 
 class HundList:
 
