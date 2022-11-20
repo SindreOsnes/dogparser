51cda21768653ab2f117b8539f1f4b7ab47ecb05
Sindre Osnes <sindreosnes.git@gmail.com>
2022-11-20 20:06:01 +0100

    Add enum indexes

--- dogparser/parsers/hund/_hund.py
+++ dogparser/parsers/hund/_hund.py
@@ -18,9 +18,31 @@ class Hund:
     _hd_id: int # hd? (HD in original schema)
     _ad_id: int # ad? (AD in original schema)
     _hem_id: int # hem? (HEM in original schema)
+    _utd_id: int # utd? (UTD in original schema)
+    _utd2_id: int # utd2? (UTD2 in original schema)
+    _utd3_id: int # utd3? (UTD3 in original schema)
+    _utmer_id: int # utmer? (UTMER in original schema)
+    _vin_id: int # vin? (VIN in original schema)
+    _k_id: int # k? (K in original schema)
+    _zb_id: int # zb? (ZB in original schema)
+    _kkl_id: int # kkl? (KKL in original schema)
+    _bruks_id: int # bruks? (BRUKS in original schema)
+    _bruks2_id: int # bruks2? (BRUKS2 in original schema)
+    _bruks3_id: int # bruks3? (BRUKS3 in original schema)
+    _bruks4_id: int # bruks4? (BRUKS4 in original schema)
+    _prem_id: int # prem? (PREM in original schema)
+    _kval_id: int # kval? (KVAL in original schema)
+    _kval2_id: int # kval2? (KVAL2 in original schema)
+    _test_id: int # test? (TEST in original schema)
+    _test2_id: int # test2? (TEST2 in original schema)
 
     def __init__(self, reg_nr: str, navn: str, fd_land_id: int, kjonn_id: int,
-                 hd_id: int, ad_id: int, hem_id: int,
+                 hd_id: int, ad_id: int, hem_id: int, utd_id: int,
+                 utd2_id: int, utd3_id: int, utmer_id: int,
+                 vin_id: int, k_id: int, zb_id: int,
+                 kkl_id: int, bruks_id: int, bruks2_id: int,
+                 bruks3_id: int, bruks4_id: int, prem_id: int,
+                 kval_id: int, kval2_id: int, test_id: int, test2_id: int,
                  fodt: Union[date, None] = None, fodt_raw: Union[str, None] = None, kullnr: Union[str, None] = None,
                  kennel: Union[str, None] = None, farge: Union[str, None] = None,) -> None:
         self._reg_nr = reg_nr
@@ -35,6 +57,23 @@ class Hund:
         self._kennel = kennel
         self._hem_id = hem_id
         self._farge = farge
+        self._utd_id = utd_id
+        self._utd2_id = utd2_id
+        self._utd3_id = utd3_id
+        self._utmer_id = utmer_id
+        self._vin_id = vin_id
+        self._k_id = k_id
+        self._zb_id = zb_id
+        self._kkl_id = kkl_id
+        self._bruks_id = bruks_id
+        self._bruks2_id = bruks2_id
+        self._bruks3_id = bruks3_id
+        self._bruks4_id = bruks4_id
+        self._prem_id = prem_id
+        self._kval_id = kval_id
+        self._kval2_id = kval2_id
+        self._test_id = test_id
+        self._test2_id = test2_id
     
     @classmethod
     def from_bytes(cls, content: bytes):
@@ -92,6 +131,74 @@ class Hund:
         # Eliminate the already used data and set the next property
         sub_content = sub_content[1:] # 142 bytes remaining
         farge = graceful_conversion(sub_content[:10]) # FARGE (color is a string capped at 10 characters)
+        
+        # Eliminate the already used data and set the next property
+        sub_content = sub_content[10:] # 132 bytes remaining
+        utd_id = sub_content[0] # UTD index
+        
+        # Eliminate the already used data and set the next property
+        sub_content = sub_content[1:] # 131 bytes remaining
+        utd2_id = sub_content[0] # UTD2 index
+        
+        # Eliminate the already used data and set the next property
+        sub_content = sub_content[1:] # 130 bytes remaining
+        utd3_id = sub_content[0] # UTD3 index
+        
+        # Eliminate the already used data and set the next property
+        sub_content = sub_content[1:] # 129 bytes remaining
+        utmer_id = sub_content[0] # UTMER index
+        
+        # Eliminate the already used data and set the next property
+        sub_content = sub_content[1:] # 128 bytes remaining
+        vin_id = sub_content[0] # VIN index
+        
+        # Eliminate the already used data and set the next property
+        sub_content = sub_content[1:] # 127 bytes remaining
+        k_id = sub_content[0] # K index
+        
+        # Eliminate the already used data and set the next property
+        sub_content = sub_content[1:] # 126 bytes remaining
+        zb_id = sub_content[0] # ZB index
+        
+        # Eliminate the already used data and set the next property
+        sub_content = sub_content[1:] # 125 bytes remaining
+        kkl_id = sub_content[0] # KKL index
+        
+        # Eliminate the already used data and set the next property
+        sub_content = sub_content[1:] # 124 bytes remaining
+        bruks_id = sub_content[0] # BRUKS index
+        
+        # Eliminate the already used data and set the next property
+        sub_content = sub_content[1:] # 123 bytes remaining
+        bruks2_id = sub_content[0] # BRUKS2 index
+        
+        # Eliminate the already used data and set the next property
+        sub_content = sub_content[1:] # 122 bytes remaining
+        bruks3_id = sub_content[0] # BRUKS3 index
+        
+        # Eliminate the already used data and set the next property
+        sub_content = sub_content[1:] # 121 bytes remaining
+        bruks4_id = sub_content[0] # BRUKS4 index
+        
+        # Eliminate the already used data and set the next property
+        sub_content = sub_content[1:] # 120 bytes remaining
+        prem_id = sub_content[0] # PREM index
+        
+        # Eliminate the already used data and set the next property
+        sub_content = sub_content[1:] # 119 bytes remaining
+        kval_id = sub_content[0] # KVAL index
+        
+        # Eliminate the already used data and set the next property
+        sub_content = sub_content[1:] # 118 bytes remaining
+        kval2_id = sub_content[0] # KVAL2 index
+        
+        # Eliminate the already used data and set the next property
+        sub_content = sub_content[1:] # 117 bytes remaining
+        test_id = sub_content[0] # TEST index
+        
+        # Eliminate the already used data and set the next property
+        sub_content = sub_content[1:] # 116 bytes remaining
+        test2_id = sub_content[0] # TEST2 index
 
         return cls(
             reg_nr=reg_nr,
@@ -106,6 +213,23 @@ class Hund:
             kennel=kennel,
             hem_id=hem_id,
             farge=farge,
+            utd_id=utd_id,
+            utd2_id=utd2_id,
+            utd3_id=utd3_id,
+            utmer_id=utmer_id,
+            vin_id=vin_id,
+            k_id=k_id,
+            zb_id=zb_id,
+            kkl_id=kkl_id,
+            bruks_id=bruks_id,
+            bruks2_id=bruks2_id,
+            bruks3_id=bruks3_id,
+            bruks4_id=bruks4_id,
+            prem_id=prem_id,
+            kval_id=kval_id,
+            kval2_id=kval2_id,
+            test_id=test_id,
+            test2_id=test2_id,
             )
     
     @property
@@ -159,12 +283,80 @@ class Hund:
     @property
     def farge(self) -> Union[str, None]:
         return None if not self._farge else self._farge
+    
+    @property
+    def utd_id(self) -> int:
+        return self._utd_id
+    
+    @property
+    def utd2_id(self) -> int:
+        return self._utd2_id
+    
+    @property
+    def utd3_id(self) -> int:
+        return self._utd3_id
+    
+    @property
+    def utmer_id(self) -> int:
+        return self._utmer_id
+    
+    @property
+    def vin_id(self) -> int:
+        return self._vin_id
+    
+    @property
+    def k_id(self) -> int:
+        return self._k_id
+    
+    @property
+    def zb_id(self) -> int:
+        return self._zb_id
+    
+    @property
+    def kkl_id(self) -> int:
+        return self._kkl_id
+    
+    @property
+    def bruks_id(self) -> int:
+        return self._bruks_id
+    
+    @property
+    def bruks2_id(self) -> int:
+        return self._bruks2_id
+    
+    @property
+    def bruks3_id(self) -> int:
+        return self._bruks3_id
+    
+    @property
+    def bruks4_id(self) -> int:
+        return self._bruks4_id
+    
+    @property
+    def prem_id(self) -> int:
+        return self._prem_id
+    
+    @property
+    def kval_id(self) -> int:
+        return self._kval_id
+    
+    @property
+    def kval2_id(self) -> int:
+        return self._kval2_id
+    
+    @property
+    def test_id(self) -> int:
+        return self._test_id
+    
+    @property
+    def test2_id(self) -> int:
+        return self._test2_id
 
     @property
     def native(self) -> dict:
         """Method converts class instance to native python classes for serialization purposes"""
 
-        return {
+        obj_dict = {
             'reg_nr': self.reg_nr,
             'navn': self.navn,
             'fodt': self.fodt_str,
@@ -177,7 +369,26 @@ class Hund:
             'kennel': self.kennel,
             'hem_id': self.hem_id,
             'farge': self.farge,
-            }
+            'utd_id':  self.utd_id,
+            'utd2_id':  self.utd2_id,
+            'utd3_id':  self.utd3_id,
+            'utmer_id':  self.utmer_id,
+            'vin_id':  self.vin_id,
+            'k_id':  self._k_id,
+            'zb_id':  self._zb_id,
+            'kkl_id':  self._kkl_id,
+            'bruks_id':  self._bruks_id,
+            'bruks2_id':  self._bruks2_id,
+            'bruks3_id':  self._bruks3_id,
+            'bruks4_id':  self._bruks4_id,
+            'prem_id':  self._prem_id,
+            'kval_id':  self._kval_id,
+            'kval2_id':  self._kval2_id,
+            'test_id':  self._test_id,
+            'test2_id':  self._test2_id,
+        }
+
+        return obj_dict
 
 class HundList:
 
