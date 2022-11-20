7a7ce8d6e220e4f829e99b3605b66417861c1287
Sindre Osnes <sindreosnes.git@gmail.com>
2022-11-20 15:34:31 +0100

    Add uncetrain conversions

--- dogparser/utils/_conversion.py
+++ dogparser/utils/_conversion.py
@@ -1,6 +1,7 @@
 def graceful_conversion(input_element: bytes) -> str:
     data_out = input_element
     data_out = sure_conversion(data_out)
+    data_out = uncertain_conversions(data_out)
     return data_out.rstrip(b'\x00').decode('utf8')
 
 def sure_conversion(input_element: bytes) -> bytes:
@@ -11,5 +12,12 @@ def sure_conversion(input_element: bytes) -> bytes:
     data_out = data_out.replace(b'\x94', b'\xc3\xb6') # ö | HUNDER BRUKS(Godkj Mögl)
     data_out = data_out.replace(b'\x9b', b'\xc3\xb8') # ø | HUNDER HD(Død)
     data_out = data_out.replace(b'\x9d', b'\xc3\x98') # Ø | HUNDER HEM(BLØDER)
+    
+    return data_out
+
+def uncertain_conversions(input_element: bytes) -> bytes:
+
+    data_out = input_element
+    data_out = data_out.replace(b'\x99', b'?X99')
     
     return data_out
