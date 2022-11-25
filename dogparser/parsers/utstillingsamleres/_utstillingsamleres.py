6ce21c772ffb9fa7a8293ea7473ac4f549f11078
Sindre Osnes <sindreosnes.git@gmail.com>
2022-11-25 01:39:26 +0100

    Add 1PREMIEANT property

--- dogparser/parsers/utstillingsamleres/_utstillingsamleres.py
+++ dogparser/parsers/utstillingsamleres/_utstillingsamleres.py
@@ -15,10 +15,11 @@ class Utstillingsamleres:
     _oppdateringsdato: Union[date, None] # Update date (OPPDATERINGSDATO in original schema)
     _oppdateringsdato_raw: Union[str, None] # Update date raw value (OPPDATERINGSDATO in original schema)
     _antgangutstilt: int # Number of times displayed (ANTGANGUTSTILT in original schema)
+    _premieant1: int # Number of times won (1PREMIEANT in original schema)
 
     def __init__(self, reg_nr: str, farens_reg_nr: Union[str, None], morfars_reg_nr: Union[str, None]
 , fodt: Union[date, None], fodt_raw: Union[str, None], kjonn_id: int, oppdateringsdato: Union[date, None], oppdateringsdato_raw: Union[str, None]
-, antgangutstilt: int) -> None:
+, antgangutstilt: int, premieant1: int) -> None:
         self._reg_nr = reg_nr
         self._farens_reg_nr = farens_reg_nr
         self._morfars_reg_nr = morfars_reg_nr
@@ -28,6 +29,7 @@ class Utstillingsamleres:
         self._oppdateringsdato = oppdateringsdato
         self._oppdateringsdato_raw = oppdateringsdato_raw
         self._antgangutstilt = antgangutstilt
+        self._premieant1 = premieant1
 
     @classmethod
     def from_bytes(cls, content: bytes):
@@ -82,6 +84,12 @@ class Utstillingsamleres:
         # Remove used data from next entries
         sub_content = sub_content[2:]
 
+        # Parse the 1PREMIEANT property
+        premieant1 = struct.unpack('H', sub_content[:2])[0] # 1PREMIEANT (Number of times won is a 2 byte integer)
+        premieant1 = None if premieant1 == 32768 else premieant1 # Set to python None if value indicates null
+        # Remove used data from next entries
+        sub_content = sub_content[2:]
+
         return cls(reg_nr=reg_nr,
                    farens_reg_nr=farens_reg_nr,
                    morfars_reg_nr=morfars_reg_nr,
@@ -90,7 +98,8 @@ class Utstillingsamleres:
                    kjonn_id=kjonn_id,
                    oppdateringsdato=oppdateringsdato,
                    oppdateringsdato_raw=oppdateringsdato_raw,
-                   antgangutstilt=antgangutstilt)
+                   antgangutstilt=antgangutstilt,
+                   premieant1=premieant1)
 
     @property
     def reg_nr(self) -> str:
@@ -137,6 +146,10 @@ class Utstillingsamleres:
         return self._antgangutstilt
 
     @property
+    def premieant1(self) -> int:
+        return self._premieant1
+
+    @property
     def native(self) -> dict:
         """Method converts class instance to native python classes for serialization purposes"""
 
@@ -150,6 +163,7 @@ class Utstillingsamleres:
             'oppdateringsdato': self.oppdateringsdato_str,
             'oppdateringsdato_raw': self.oppdateringsdato_raw,
             'antgangutstilt': self.antgangutstilt,
+            'premieant1': self.premieant1,
         }
 
         return obj_dict
